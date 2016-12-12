2d94558e1b331c0bb6bfd8b8c7668cbccf00bbf7
Anders <andersmurphy@gmail.com>
2016-12-12 09:43:35 +0000

    adds socket.io and connects to server

--- src/index.jsx
+++ src/index.jsx
@@ -5,33 +5,18 @@ import {createStore} from 'redux'
 import reducer from './reducer'
 import {Map, List} from 'immutable'
 import {Provider} from 'react-redux'
+import io from 'socket.io-client'
 import App from './components/App'
 import {QuestionContainer} from './components/Question'
 import Start from './components/Start'
 import Finish from './components/Finish'
 
 const store = createStore(reducer)
-store.dispatch({
-  type: 'SET_STATE',
-  state: Map({
-    questions: List.of(Map({
-          question: 'question2',
-          answers: Map({'answerA': 0,'answerB': 5,'answerC': 10,'answerD': 15})
-        }),
-        Map({
-          question: 'question3',
-          answers: Map({'answerA': 0,'answerB': 5,'answerC': 10,'answerD': 15})
-        })),
-    current_question: Map({
-        question: 'question1',
-        answers: Map({'answerA': 0,'answerB': 5,'answerC': 10,'answerD': 15}),
-        selected_answer: 'answerC'
-      }),
-    name: 'John',
-    total_score: 25,
-    max_score: 100
-    })
-  })
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`)
+socket.on('state', state =>
+  store.dispatch({type: 'SET_STATE', state})
+)
 
 const routes = <Route component={App}>
   <Route path="/" component={QuestionContainer} />
